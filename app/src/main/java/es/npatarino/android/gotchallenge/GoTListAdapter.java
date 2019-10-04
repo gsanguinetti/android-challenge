e070f1c02e4b775703f5f9334876af8eb48f3518
Gaston Sanguinetti <gaston.sanguinetti@mercadolibre.com>
2019-10-04 20:37:03 -0300

    Upgrade sdk version and androidX

--- app/src/main/java/es/npatarino/android/gotchallenge/GoTListAdapter.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/GoTListAdapter.java
@@ -1,14 +1,12 @@
 package es.npatarino.android.gotchallenge;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.List;
 
 /**
  * Created by Admin on 15/12/2016.
